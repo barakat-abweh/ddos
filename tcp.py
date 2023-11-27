da47b3609c4f02f04df85ffe7b57a97b254d7cd8
amrjaara <93577651+amrjaara@users.noreply.github.com>
2023-11-27 09:59:24 +0200

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -8,12 +8,3 @@ def send_udp_packet(source_port, destination_ip, destination_port, message):
 
     # Send the packet
     send(packet)
-
-# Example usage
-#source_port = 53  # Replace with your desired source port
-#destination_ip = '192.168.180.13'  # Replace with the destination IP
-#destination_port = 53  # Replace with the destination port
-#message = "Hello, UDP!"  # Replace with your message
-#for i in range(5000000): 
- #send_udp_packet(source_port, destination_ip, destination_port, message)
-
