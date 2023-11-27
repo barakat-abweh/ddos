275a01a0a70f16bfd9fc2ac7fbf2a3530ca336de
barakat-abweh <b.abweh@protonmail.com>
2023-11-27 10:27:52 +0200

    modified tcp.py

--- tcp.py
+++ tcp.py
@@ -1,11 +1,10 @@
 #!/bin/python3
 
-from scapy.all import IP, UDP, send
+from scapy.all import IP, TCP, send
 
-def send_udp_packet(source_port, destination_ip, destination_port, message):
-    # Create an IP packet with UDP payload
-    packet = IP(dst=destination_ip) / UDP(sport=source_port, dport=destination_port) / message
+def send_custom_tcp_packet(source_port, destination_ip, destination_port, flags, message):
+    # Create an IP packet with TCP payload and custom flags
+    packet = IP(dst=destination_ip) / TCP(sport=source_port, dport=destination_port, flags=flags) / message
 
     # Send the packet
     send(packet)
-#fghjkl;'lkjkl;kjhgj
